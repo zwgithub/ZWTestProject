ace81804ccffc2aca16503fd8c0bf58b6747aa75
zwgithub <caozhenwei8888@gmail.com>
2016-04-24 23:56:20 +0800

    add toolbar

---
+++ ZWTestProject/Refrence/ZWPhotoBrowser/ZWToolbar.h
@@ -0,0 +1,13 @@
+//
+//  ZWToolbar.h
+//  ZWTestProject
+//
+//  Created by shanWu on 16/4/24.
+//  Copyright © 2016年 caozhenwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZWToolbar : UIView
+
+@end
---
+++ ZWTestProject/Refrence/ZWPhotoBrowser/ZWToolbar.m
@@ -0,0 +1,21 @@
+//
+//  ZWToolbar.m
+//  ZWTestProject
+//
+//  Created by shanWu on 16/4/24.
+//  Copyright © 2016年 caozhenwei. All rights reserved.
+//
+
+#import "ZWToolbar.h"
+
+@implementation ZWToolbar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
