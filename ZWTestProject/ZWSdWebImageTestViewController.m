7b0de919545250dd7a8992ed2fa155f508b95df0
曹振伟 <caozhenwei8888@gmail.com>
2016-04-27 13:45:44 +0800

    修改写错的地方

--- ZWTestProject/ZWSdWebImageTestViewController.m
+++ ZWTestProject/ZWSdWebImageTestViewController.m
@@ -17,11 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+    
     NSString *str = @"https://raw.githubusercontent.com/zwgithub/blog_pic/master/animal_city4.jpg";
     NSURL *url = [NSURL URLWithString:str];
     [[SDWebImageManager sharedManager] downloadImageWithURL:url options:SDWebImageLowPriority | SDWebImageRetryFailed progress:^(NSInteger receivedSize, NSInteger expectedSize) {
@@ -29,6 +25,10 @@
     } completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
         NSLog(@"completed");
     }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
 }
 
 @end
